55986b916cd9ba328f957d6e176eb2efcad4199b
Tushar Godse <tusharg@bitsdev.in>
2020-04-20 20:00:33 +0530

    init

---
+++ src/main/java/com/bits/std/xsdtojavausingjaxbplugin/App.java
@@ -0,0 +1,17 @@
+package com.bits.std.xsdtojavausingjaxbplugin;
+
+import com.example.myschema.Note;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    Note n ;
+}
